56db46dd61e074a1c484b27eb6e609f20e878e3b
JaySoftye <jonathansoftye@teq.com>
2020-08-17 17:10:54 -0400

    Updates
    Still in Development

---
+++ template-pages/teqtips.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Template Name: Teq Tips
+ * The template for displaying all teq tips posts
+ * Post of category Media > Teq Tips
+ * @package Teq_v4.0
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area" scroll>
+	<main id="main" class="site-main section-container">
+
+		<section class="full-section block">
+			<div class="container page-content">
+				<div class="columns is-vcentered is-desktop">
+					<div class="column is-6 is-offset-3">
+						<h1 class="page-titles has-text-centered">
+							<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 60 21">
+								<polygon points="15.658,5.519 10.17,5.266 10.423,6.363 12.112,6.194 11.943,15.059 12.618,15.228 13.547,6.11 15.404,6.025 	"/>
+								<path d="M18.275,13.455c-0.084,0-0.169,0-0.253,0c-0.929,0.084-2.111-0.253-2.448-2.448l3.968-0.253 c0-0.338-0.084-0.591-0.084-0.844c-0.253-1.52-0.929-2.702-2.617-2.617c-0.084,0-0.169,0-0.169,0 c-1.689,0.253-2.28,2.026-1.942,4.052c0.253,1.604,1.266,3.461,3.715,3.039c0.591-0.084,1.182-0.253,1.435-0.422l-0.507-0.929 C19.119,13.202,18.782,13.37,18.275,13.455z M16.671,8.052c0.844-0.084,1.435,0.591,1.604,1.942l-2.702,0.338 C15.573,9.318,15.742,8.22,16.671,8.052z"/>
+								<path d="M25.367,8.643L25.367,8.643c-0.338-0.76-0.929-1.098-1.52-1.182c-1.266-0.169-2.702,0.844-3.039,3.039 c-0.338,1.773-0.084,3.884,1.857,4.221c0.844,0.084,1.604-0.338,1.942-0.76h0.084l-0.422,3.546l0.844-0.084l1.351-7.261 l0.338-2.111l-1.351-0.591L25.367,8.643z M25.029,10.331L24.86,12.02c-0.169,1.266-1.098,1.689-1.604,1.604 c-1.351-0.169-1.435-2.026-1.266-3.208c0.253-1.351,0.929-2.28,1.942-2.195C24.691,8.389,25.114,9.318,25.029,10.331z"/>
+								<polygon points="31.277,7.376 33.809,7.376 33.809,14.299 35.498,14.299 35.498,7.376 38.031,7.376 38.031,6.025 31.277,6.025 	"/>
+								<path d="M39.213,5.688c-0.253,0-0.507,0.084-0.675,0.253c-0.169,0.169-0.253,0.338-0.253,0.591c0,0.253,0.084,0.422,0.253,0.591 c0.169,0.169,0.422,0.253,0.675,0.253c0.253,0,0.507-0.084,0.675-0.253c0.169-0.169,0.253-0.338,0.253-0.591 c0-0.253-0.084-0.422-0.253-0.591C39.719,5.772,39.55,5.688,39.213,5.688z"/>
+								<rect x="38.453" y="8.136" width="1.604" height="6.163"/>
+								<path d="M43.856,8.052c-0.675,0-1.182,0.253-1.52,0.675l-0.084-0.591h-1.52v8.527h1.604v-2.87c0.422,0.422,0.844,0.675,1.52,0.675 c0.76,0,1.351-0.253,1.773-0.844c0.422-0.591,0.675-1.351,0.675-2.28V11.26c0-1.013-0.169-1.773-0.591-2.28 C45.207,8.305,44.616,8.052,43.856,8.052z M43.434,13.117c-0.507,0-0.844-0.169-1.098-0.591V9.993 c0.169-0.422,0.507-0.591,1.098-0.591c0.422,0,0.675,0.169,0.844,0.507c0.169,0.338,0.338,0.76,0.338,1.351 C44.616,12.442,44.194,13.117,43.434,13.117z"/>
+								<path d="M50.695,10.922c-0.253-0.084-0.591-0.169-1.013-0.253c-0.422-0.084-0.675-0.169-0.844-0.253s-0.253-0.253-0.253-0.422 s0.084-0.338,0.253-0.507c0.169-0.084,0.338-0.169,0.675-0.169s0.591,0.084,0.675,0.253c0.084,0.169,0.253,0.338,0.253,0.591h1.604 c0-0.591-0.253-1.098-0.675-1.435c-0.507-0.338-1.098-0.591-1.857-0.591c-0.76,0-1.351,0.169-1.773,0.507 c-0.422,0.338-0.675,0.844-0.675,1.351c0,0.929,0.76,1.52,2.195,1.857c0.422,0.084,0.76,0.169,0.929,0.338 c0.169,0.084,0.338,0.253,0.338,0.507c0,0.169-0.084,0.338-0.253,0.507c-0.169,0.084-0.422,0.169-0.675,0.169 c-0.338,0-0.591-0.084-0.844-0.253c-0.169-0.169-0.338-0.338-0.338-0.675h-1.52c0,0.338,0.084,0.675,0.338,1.013 c0.253,0.338,0.507,0.591,0.929,0.76c0.422,0.169,0.844,0.253,1.351,0.253c0.76,0,1.351-0.169,1.857-0.507 c0.507-0.338,0.76-0.76,0.76-1.351C51.961,11.766,51.539,11.175,50.695,10.922z"/>
+								<rect x="28.997" y="0.622" width="0.253" height="19.756"/>
+							</svg>
+						</h1>
+					</div>
+				</div>
+			</div>
+
+			<div class="container padding-bottom padding-top">
+				<div class="columns is-vcentered is-centered is-desktop post-card-container">
+					<?php
+						$args = array(
+							'category_name' => 'Teq Tips',
+							'posts_per_page' => 1
+						);
+
+						$the_query = new WP_Query( $args );
+							if ( $the_query->have_posts() ) :
+								while ( $the_query->have_posts() ) :
+									$the_query->the_post();
+					?>
+					<article class="column is-10">
+						<h2 class="strong"><?php the_title(); ?></h2>
+						<p><?php the_excerpt(); ?></p>
+						<div class="drop-shadow"><?php the_content(); ?></div>
+					</article>
+					<?php endwhile; wp_reset_postdata(); endif; ?>
+				</div>
+			</div>
+
+			<div class="container padding-bottom">
+				<div class="columns is-centered is-desktop">
+					<div class="column is-10 teq-blog-search">
+
+
+						<h5>Teq Tips hosts an entire library of useful classroom videos. Using the form field below feel free to <strong>search for a specific subject matter or product.</strong></h5>
+						<form role="search" method="get" class="teq-blog-search search-form" action="<?php echo home_url(); ?>">
+							<div class="field has-addons">
+								<div class="control">
+									<input type="search" class="input search-field" placeholder="Search …" value="" name="s">
+								</div>
+								<input type="hidden" value="category_name" name="teq-tips" />
+								<input type="hidden" name="post_type" value="post" />
+								<div class="control">
+									<button type="submit" class="search-submit search-button" value="Search"></button>
+								</div>
+							</div>
+						</form>
+
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="recent-posts-container padding-bottom">
+			<div class="container">
+				<div class="columns is-centered padding-bottom">
+					<div class="column is-10">
+						<div class="columns">
+							<div class="column is-full border-bottom">
+								<h4>You might also like:</h4>
+							</div>
+						</div>
+						<div class="columns is-desktop is-multiline">
+
+							<?php
+				   		// recent post query
+								$the_query = new WP_Query( array(
+									'post_type' => 'post',
+									'orderby'   => 'rand',
+				     			'category_name' => 'teq-tips',
+				      		'posts_per_page' => 6
+				   			));
+							?>
+
+							<?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+								<div class="column is-4">
+									<div class="card">
+									<h6 class="has-text-centered strong padding-sm"><?php the_title(); ?></h6>
+									<div class="drop-shadow"><?php the_content(); ?></div>
+									</div>
+								</div>
+				  			<?php endwhile; ?>
+				  		<?php wp_reset_postdata(); endif; ?>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_footer();
